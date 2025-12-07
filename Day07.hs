d902957d4efa06d9f4b373fe915fce3413546066
Jamie Curtis <jamocurtis@gmail.com>
2025-12-07 12:49:13 -0400

    day 7 part 2

--- Day07.hs
+++ Day07.hs
@@ -1,10 +1,12 @@
 import Data.List (elemIndex, elemIndices)
 import Data.List.Extra (nub)
 import Data.Maybe (fromJust)
+import Data.MemoTrie (memo2)
 
 main = do
   input <- getContents
   print (solve input)
+  print (solve' input)
 
 testInput = ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n..............."
 
@@ -12,6 +14,8 @@ parse input = (start, splitters)
   where
     start = fromJust $ elemIndex 'S' input
     splitters = map (elemIndices '^') (lines input)
+
+-- part 1
 
 solve input =
   snd $
@@ -37,3 +41,26 @@ splitBeam splitterLine beam
   | otherwise = [beam]
 
 test = solve testInput == 21
+
+-- part 2
+
+teleport' beams [] = length beams
+teleport' [] _ = 0
+teleport' beams (splitterLine : splitters) =
+  sum $
+    map
+      (splitBeam'Memo (splitterLine : splitters))
+      beams
+
+splitBeam' :: [[Int]] -> Int -> Int
+splitBeam' (splitterLine : splitters) beam
+  | beam `elem` splitterLine = teleport' [beam - 1, beam + 1] splitters
+  | otherwise = teleport' [beam] splitters
+
+splitBeam'Memo = memo2 splitBeam'
+
+solve' input = teleport' [start] splitters
+  where
+    (start, splitters) = parse input
+
+test' = solve' testInput == 40
